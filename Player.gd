93f0270505a8f7e8e6c1e1c7bcfb9572df770f75
DwarfNinja <cenduroyib@gmail.com>
2020-05-19 23:32:50 +0200

    First manual commit
    Movement system, background and rocks added

---
+++ Player.gd
@@ -0,0 +1,29 @@
+extends KinematicBody2D
+
+const ACCELERATION = 800
+const MAX_SPEED = 80
+const FRICTION = 600
+
+var velocity = Vector2.ZERO
+
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("key_right") - Input.get_action_strength("key_left")
+	input_vector.y = Input.get_action_strength("key_down") - Input.get_action_strength("key_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	velocity = move_and_slide(velocity)
