5f7beb9c179e2ef4ba6c9fa8f68ffa1dbedc0fc4
DwarfNinja <cenduroyib@gmail.com>
2020-06-30 01:33:14 +0200

    Random-Generation Ver.1.2
    Places random first room and places a new random room next to it making sure it fits correctly. Determines room viability based on first tile

--- Scripts/House/RandomGenHouse.gd
+++ Scripts/House/RandomGenHouse.gd
@@ -1,49 +1,73 @@
 extends Node2D
 
-onready var CROSS_SHAPE = get_node("HouseShapes/Cross/Walls")
-#var L_DOWN_SHAPE = load("res://Scenes/House/HouseShapes/L-Down/Walls.tscn")
-onready var L_DOWN_SHAPE = get_node("HouseShapes/L_Down/Walls")
-onready var L_UP_SHAPE = get_node("HouseShapes/L_Up/Walls")
-onready var R_DOWN_SHAPE = get_node("HouseShapes/R_Down/Walls")
-onready var R_UP_SHAPE = get_node("HouseShapes/R_Up/Walls")
+onready var CROSS_SHAPE = get_node("HouseShapes/Cross")
+onready var L_DOWN_SHAPE = get_node("HouseShapes/L_Down")
+onready var L_UP_SHAPE = get_node("HouseShapes/L_Up")
+onready var R_DOWN_SHAPE = get_node("HouseShapes/R_Down")
+onready var R_UP_SHAPE = get_node("HouseShapes/R_Up")
 
 var rooms = 0
-var max_rooms = 4
+var max_rooms = 2
 var rooms_generated = false
+var first_room_placed = false
+var first_room_data
+var new_room_location
 
-var pos_for_room_0 = Vector2(0,0)
-var pos_for_room_1 = Vector2(22, 0)
-var pos_for_room_2 = Vector2(0, 22)
-var pos_for_room_3 = Vector2(22, 22)
+var first_room_pos = Vector2(0,0)
+var room_pos_0 = Vector2(0, -22)
+var room_pos_1 = Vector2(22, 0)
+var room_pos_2 = Vector2(0, 22)
+var room_pos_3 = Vector2(-22, 0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
 	
 func _process(_delta):
-	if rooms < max_rooms:
-		var random_shape = choose_random_shape([CROSS_SHAPE, L_DOWN_SHAPE, L_UP_SHAPE, R_DOWN_SHAPE, R_UP_SHAPE])
-		var room_data = random_shape.get_parent().get_room_data()
-		var last_room_data = room_data[0]
-		var new_room_data = room_data[1]
-		for i in new_room_data:
-			if i in last_room_data:
-				set_random_room(random_shape)
-			else:
-				random_shape = choose_random_shape([CROSS_SHAPE, L_DOWN_SHAPE, L_UP_SHAPE, R_DOWN_SHAPE, R_UP_SHAPE])
+	if first_room_placed == false:
+		place_first_room()
 		
-		set_random_room(random_shape)
-		rooms += 1
+	if first_room_placed == true and rooms < max_rooms:
+		check_randomroom_viability()
+		
 	if rooms == max_rooms and rooms_generated == false:
 		update_allcell_bitmasks()
 		
+func set_random_room(random_shape_walls, random_shape_floor):
+	for cell in random_shape_walls.get_used_cells():
+		$Walls.set_cell(cell.x + get("room_pos_" + str(new_room_location)).x, 
+		cell.y + get("room_pos_" + str(new_room_location)).y, random_shape_walls.get_cellv(cell), 
+		false, false, false, random_shape_walls.get_cell_autotile_coord(cell.x, cell.y))
+	for cell in random_shape_floor.get_used_cells():
+		$Floor.set_cell(cell.x + get("room_pos_" + str(new_room_location)).x/2, 
+		cell.y + get("room_pos_" + str(new_room_location)).y/2, random_shape_floor.get_cellv(cell), 
+		false, false, false, random_shape_floor.get_cell_autotile_coord(cell.x, cell.y))
+	rooms += 1
 		
-func set_random_room(random_shape):
-	for cell in random_shape.get_used_cells():
-#		$Walls.set_cellv(cell, random_shape.get_cellv(cell))
-		$Walls.set_cell(cell.x + get("pos_for_room_" + str(rooms)).x, cell.y + get("pos_for_room_" + str(rooms)).y, random_shape.get_cellv(cell), 
-		false, false, false, random_shape.get_cell_autotile_coord(cell.x, cell.y))
-#		$Walls.update_bitmask_area(cell)
+		
+func place_first_room():
+	var first_shape = choose_random_shape([CROSS_SHAPE, L_DOWN_SHAPE, L_UP_SHAPE, R_DOWN_SHAPE, R_UP_SHAPE])
+	var first_shape_walls = first_shape.get_node("Walls")
+	first_room_data = first_shape.get_room_data()[0]
+	for cell in first_shape_walls.get_used_cells():
+		$Walls.set_cell(cell.x + first_room_pos.x, cell.y + first_room_pos.y, first_shape_walls.get_cellv(cell), 
+		false, false, false, first_shape_walls.get_cell_autotile_coord(cell.x, cell.y))
+	rooms += 1
+	first_room_placed = true
+
+func check_randomroom_viability():
+	var random_shape = choose_random_shape([CROSS_SHAPE, L_DOWN_SHAPE, L_UP_SHAPE, R_DOWN_SHAPE, R_UP_SHAPE])
+	var random_shape_walls = random_shape.get_node("Walls")
+	var random_shape_floor = random_shape.get_node("Floor")
+	var new_room_data = random_shape.get_room_data()[1]
+	for i in range(0, first_room_data.size() -1):
+		var value1 = first_room_data[i]
+		var value2 = new_room_data[i]
+		if value1 == value2:
+			if value1 == true and value2 == true:
+				new_room_location = i
+				set_random_room(random_shape_walls, random_shape_floor)
+				break
 
 func update_allcell_bitmasks():
 	for cell in $Walls.get_used_cells():
--- Scripts/House/Shape.gd
+++ Scripts/House/Shape.gd
@@ -5,15 +5,12 @@ export (bool) var opening_east
 export (bool) var opening_south
 export (bool) var opening_west
 
+var connect_north = false
+var connect_east = false
+var connect_south = false
+var connect_west = false
+
 func get_room_data():
-	var connect_north = false
-	var connect_east = false
-	var connect_south = false
-	var connect_west = false
-	
-	var shape_openings = [opening_north, opening_east, opening_south, opening_west]
-	var viable_connections = [connect_north, connect_east, connect_south, connect_west]
-	
 	if opening_north == true:
 		connect_south = true
 	if opening_east == true:
@@ -21,6 +18,9 @@ func get_room_data():
 	if opening_south == true:
 		connect_north = true
 	if opening_west == true:
-		connect_south = true
-		
+		connect_east = true
+	
+	var shape_openings = [opening_north, opening_east, opening_south, opening_west]
+	var viable_connections = [connect_north, connect_east, connect_south, connect_west]
+	
 	return([shape_openings,viable_connections])
