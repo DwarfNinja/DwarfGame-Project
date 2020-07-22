72dca2760538629df089060f3a661a0bea157fb4
DwarfNinja <cenduroyib@gmail.com>
2020-07-23 00:10:49 +0200

    Random-Generation Ver1.3.2
    Added fill_with_walls() and location_of_value(). Made adjustments for new grid system and cleanup

--- Scripts/House/V1.1-RandomGenHouse.gd
+++ Scripts/House/V1.1-RandomGenHouse.gd
@@ -5,6 +5,8 @@ onready var L_DOWN_SHAPE = get_node("HouseShapes/L_Down")
 onready var L_UP_SHAPE = get_node("HouseShapes/L_Up")
 onready var R_DOWN_SHAPE = get_node("HouseShapes/R_Down")
 onready var R_UP_SHAPE = get_node("HouseShapes/R_Up")
+onready var SQUARESPACE_SW_E = get_node("HouseShapes/SquareSpace_SW-E")
+onready var ZIG_NW_S = get_node("HouseShapes/Zig_NW-S")
 
 var rooms = 0
 var max_rooms = 2
@@ -12,8 +14,7 @@ var rooms_generated = false
 var first_room_placed = false
 var first_room_data
 var possible_new_room_locations = []
-var new_random_room_location
-var checking_rooms = false
+var checking_room = false
 
 var first_room_pos = Vector2(0,0)
 var room_pos_0 = Vector2(0, -20)
@@ -26,15 +27,18 @@ func _ready():
 	randomize()
 	
 func _process(_delta):
+	if Input.is_action_just_pressed("key_r"):
+		get_tree().reload_current_scene()
+	if Input.is_action_just_pressed("key_e"):
+		fill_with_walls()
 	if first_room_placed == false:
 		set_first_room()
 		
-	if first_room_placed == true and rooms < max_rooms and checking_rooms == false:
+	if first_room_placed == true and rooms < max_rooms and checking_room == false:
 		check_randomroom_viability()
 		
 	if rooms == max_rooms and rooms_generated == false:
 		update_allcell_bitmasks()
-		
 		
 func set_first_room():
 	var first_shape = choose_random_shape()
@@ -49,7 +53,7 @@ func set_first_room():
 	first_room_placed = true
 		
 		
-func set_random_room(random_shape_walls, random_shape_floor):
+func set_random_room(random_shape_walls, random_shape_floor, new_random_room_location):
 	for cell in random_shape_walls.get_used_cells():
 		$Walls.set_cell(cell.x + get("room_pos_" + str(new_random_room_location)).x, 
 		cell.y + get("room_pos_" + str(new_random_room_location)).y, random_shape_walls.get_cellv(cell), 
@@ -68,20 +72,21 @@ func check_randomroom_viability():
 	var new_room_data = random_shape.get_room_data()[1]
 	
 	var count = 0
-	checking_rooms = true
+	checking_room = true
 	for i in range(0, first_room_data.size()):
 		count += 1
 		var value1 = first_room_data[i]
 		var value2 = new_room_data[i]
 		if value1 == value2:
 			if value1 == true and value2 == true:
-				possible_new_room_locations.append(i)
+				possible_new_room_locations.append(location_of_value(i))
 	
-	if count == 4 and possible_new_room_locations != []:
-		new_random_room_location = choose_random_location(possible_new_room_locations)
-		set_random_room(random_shape_walls, random_shape_floor)
+	if count == 12 and possible_new_room_locations != []:
+		
+		var new_random_room_location = choose_random_location(possible_new_room_locations)
+		set_random_room(random_shape_walls, random_shape_floor, new_random_room_location)
 	else:
-		checking_rooms = false
+		checking_room = false
 		
 
 func update_allcell_bitmasks():
@@ -89,8 +94,25 @@ func update_allcell_bitmasks():
 		$Walls.update_bitmask_area(cell)
 	rooms_generated = true
 
+func fill_with_walls(rect = Rect2(-40, -40, 80, 80)):
+	for y in range(rect.position.y, rect.end.y):
+		for x in range(rect.position.x, rect.end.x):
+			if $Floor.get_cell(x,y) == -1:
+				$Walls.set_cell(x, y, 0)
+
+func location_of_value(value):
+	if value in range(0,3):
+		value = 0
+	elif value in range(3,6):
+		value = 1
+	elif value in range(6,9):
+		value = 2
+	elif value in range(9,12):
+		value = 3
+	return value
+
 func choose_random_shape():
-	var house_shape_list = [CROSS_SHAPE, L_DOWN_SHAPE, L_UP_SHAPE, R_DOWN_SHAPE, R_UP_SHAPE]
+	var house_shape_list = [CROSS_SHAPE, L_DOWN_SHAPE, L_UP_SHAPE, R_DOWN_SHAPE, R_UP_SHAPE, SQUARESPACE_SW_E, ZIG_NW_S]
 	house_shape_list.shuffle()
 	return house_shape_list.pop_front()
 	
