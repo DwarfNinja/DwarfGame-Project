5e9dd043e895372fc055276c4819847b68affedb
DwarfNinja <cenduroyib@gmail.com>
2021-01-28 02:17:22 +0100

    Added Testroom, commit before full Tileset change
    Commit before totally reworking Tilesets and Rooms.

--- Scripts/Worlds/House/V.1.35-RandomGenHouse.gd
+++ Scripts/Worlds/House/V.1.35-RandomGenHouse.gd
@@ -11,6 +11,8 @@ onready var SW_RIGHTANGLE = get_node("HouseRooms/RightAngle/S-W_RightAngle")
 onready var SQUARESPACE_SW_E = get_node("HouseRooms/SquareSpace_SW-E")
 onready var ZIG_NW_S = get_node("HouseRooms/Zig_NW-S")
 
+onready var TEST_ROOM = get_node("HouseRooms/TestRoom")
+
 onready var SQUARE_4x5 = get_node("HouseShapes/Square_4x2")
 onready var SE_5x2 = get_node("HouseShapes/SE_3x2")
 onready var Hor_4x1 = get_node("HouseShapes/Hor_4x1")
@@ -48,7 +50,7 @@ var room_pos_west = Vector2(-20, 0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-
+	
 	randomize()
 	
 	if max_rooms == 2:
@@ -255,8 +257,9 @@ func get_tiles_in_rectangle(node_pos, rect_width, rect_height):
 			rect_tiles.append(Vector2(x, y)) # or get_cell(x, y) or however you want it represented
 #	print("RECT_TILES = ", rect_tiles)
 	return rect_tiles
-	
-# Look at code, can be imporved line 262-265
+
+
+#TODO: Look at code, can be imporved line 262-265
 func place_items():
 	var item_tile_array = $Nav2D/Indexes.get_used_cells_by_id(13)
 	var random_item_positions = select_random_item_positions(item_tile_array)
@@ -271,8 +274,8 @@ func place_items():
 		$Nav2D/Walls.add_child(random_item)
 		$Nav2D/Indexes.set_cellv(random_item_positions[i], -1)
 		$Nav2D/Area.set_cellv(random_item_positions[i], -1)
-	
-	
+
+
 func place_enemies():
 	var enemy_tile_array = $Nav2D/Indexes.get_used_cells_by_id(12)
 	var random_enemy_positions = select_random_enemy_positions(enemy_tile_array)
@@ -284,8 +287,8 @@ func place_enemies():
 		random_enemy.set_position(tilepos + Vector2(7.99, 8.01)) #Bug in Nav2D where it rounds up ints?
 		$Nav2D/Walls.add_child(random_enemy)
 		$Nav2D/Indexes.set_cellv(random_enemy_positions[i], -1)
-		
-		
+
+
 func place_player_spawn():
 	var spawn_tile_array = $Nav2D/Indexes.get_used_cells_by_id(14)
 	var random_spawn_position = select_random_spawn_position(spawn_tile_array)
@@ -297,16 +300,21 @@ func place_player_spawn():
 	$Nav2D/Walls/Player/AnimationTree.set("parameters/Idle/blend_position", Vector2(0,-0.1))
 	for i in range(0, spawn_tile_array.size()):
 		$Nav2D/Indexes.set_cell(spawn_tile_array[i].x, spawn_tile_array[i].y, -1)
-		
+
+
 func place_door(random_spawn_position, tilepos):
 	var door = DOOR_SCENE.instance()
 	$Nav2D/Walls.set_cell(random_spawn_position.x, random_spawn_position.y, -1)
 	$Nav2D/Walls.set_cell(random_spawn_position.x + 1 , random_spawn_position.y, -1)
 	$Nav2D/Walls.add_child(door)
 	door.set_position(tilepos)
+	place_spawn_zone(door)
+
+
+func place_spawn_zone(door):
+	# Position offset to compensate so the zone isn't in the wall
 	var spawn_zone_width = 15
 	var spawn_zone_height = 8
-	# Door position offset to compensate so the zone isn't in the wall
 	var spawn_zone = get_tiles_in_rectangle(
 		$Nav2D/Walls.world_to_map(door.position) - 
 		Vector2(0,ceil(spawn_zone_height/2) + 1), spawn_zone_width, spawn_zone_height)
@@ -314,7 +322,7 @@ func place_door(random_spawn_position, tilepos):
 	for cell in spawn_zone:
 		if $Nav2D/Indexes.get_cellv(cell) != -1:
 			$Nav2D/Indexes.set_cell(cell.x, cell.y, -1)
-		
+
 
 func location_of_value(converted_randomroom_location):
 	var locations = {
@@ -339,7 +347,7 @@ func location_of_value(converted_randomroom_location):
 	
 func select_random_room():
 	var room_list = [CROSS, NE_RIGHTANGLE, SE_RIGHTANGLE, NW_RIGHTANGLE,
-	 SW_RIGHTANGLE, SQUARESPACE_SW_E, ZIG_NW_S]
+	 SW_RIGHTANGLE, SQUARESPACE_SW_E, ZIG_NW_S, TEST_ROOM]
 	room_list.shuffle()
 	return room_list.pop_front()
 	
