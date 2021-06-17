e826c63e0beaa451eb0162682cc244f579d44a3c
DwarfNinja <cenduroyib@gmail.com>
2021-06-17 23:15:10 +0200

    Split footprint into tile_footprint and collision_footprint

--- Scripts/Resources/R_Entity.gd
+++ Scripts/Resources/R_Entity.gd
@@ -1,5 +1,6 @@
 extends R_Item
 class_name R_Entity
 
-export (Vector2) var footprint
+export (Vector2) var tile_footprint
+export (Vector2) var collision_footprint
 export (Texture) var entity_texture
